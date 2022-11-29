4eb864a42478c503b88e16debc8c64b718bba0ab
Lawrence Li <lzli@usc.edu>
2022-11-29 06:23:44 +0000

    final

--- schedwork.cpp
+++ schedwork.cpp
@@ -12,13 +12,22 @@
 
 using namespace std;
 
-// a constant that can be used to indicate an INVALID 
+// a constant that can be used to indicate an INVALID
 // worker ID if that is useful to your implementation.
 // Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool helper(const AvailabilityMatrix& avail,
+            const size_t maxShifts,
+            DailySchedule& sched,
+            vector<int> shiftsRecord,
+            int currentDay,
+            vector<vector<Worker_T>>
+);
+
+vector<vector<Worker_T>> combinations(int workers, int dailyNeeded);
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -34,10 +43,94 @@ bool schedule(
         return false;
     }
     sched.clear();
-    // Add your code below
+    int days = avail.size();
+    int workers = avail.front().size();
+    vector<int> shifts;
 
+    sched.resize(days);
+    shifts.resize(workers);
 
+    //All combinations of workers, i.e. total workers choose needed workers
+    vector<vector<Worker_T>> combos = combinations(workers, dailyNeed);
 
-
+    //Backtracking function
+    return helper(avail, maxShifts, sched, shifts, 0, combos);
 }
 
+
+//Backtracking helper function
+bool helper(const AvailabilityMatrix& avail,
+            const size_t maxShifts,
+            DailySchedule& sched,
+            vector<int> shiftsRecord,
+            int currentDay,
+            vector<vector<Worker_T>> combos)
+{
+
+    if(currentDay == avail.size())                                                          //Reached the end
+    {
+        return true;
+    }
+
+    //Iterating through every worker combinations to see if it can fit on that day
+    for(int i = 0; i < combos.size(); i++)
+    {
+        bool exceeded = false;
+        vector<Worker_T> temp = combos[i];
+        //Iterating through each worker
+        for(int j = 0; j < temp.size(); j++)
+        {
+            //Member is unavaliable/maxshifts. Combination fails.
+            if(shiftsRecord[temp[j]] == maxShifts || avail[currentDay][temp[j]] == 0)
+            {
+                exceeded = true;
+                break;
+            }
+        }
+        if(exceeded)
+        {
+            continue;
+        }
+
+        //Update shifts taken
+        for(int j = 0; j < temp.size(); j++)
+        {
+            shiftsRecord[temp[j]]++;
+        }
+
+        sched.at(currentDay) = temp;
+
+        //Next Backtracking iteration
+        if(helper(avail, maxShifts, sched, shiftsRecord, currentDay + 1, combos))
+            return true;
+
+        //Backtracking fails, update shifts taken again
+        for(int j = 0; j < temp.size(); j++)
+        {
+            shiftsRecord[temp[j]]--;
+        }
+    }
+
+    return false;
+}
+
+
+//Function for finding all needed combination of workers, i.e. total workers choose needed workers
+vector<vector<Worker_T>> combinations(int workers, int dailyNeed)
+{
+
+    vector<vector<Worker_T>> sol;
+    vector<bool> v(workers);
+    fill(v.end() - dailyNeed, v.end(), true);
+
+    do {
+        vector<Worker_T> temp;
+        for (int i = 0; i < workers; ++i) {
+            if (v[i]) {
+                temp.push_back(i);
+            }
+        }
+        sol.push_back(temp);
+    } while (next_permutation(v.begin(), v.end()));
+    return sol;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -23,6 +23,8 @@ std::set<std::string> wordle(
     string intemp = in;
     string::iterator stritr = floatingtemp.begin();
     set<int> inlist;
+    
+    //First for-loop to gauge empty spaces and merge strings
     for(int i = 0; i < intemp.size(); i++)
     {
         if(intemp[i] == '-')
@@ -43,20 +45,30 @@ std::set<std::string> wordle(
             inlist.insert(i);
         }
     }
-
-    if(stritr != floatingtemp.end())           //iterator never reaches end
+    
+    if(stritr != floatingtemp.end())           
     {
         throw out_of_range("Too many floating characters");
     }
 
+    
     set<string> words;
+    
+    //To fill empty spaces with alphabetical combinations of the original string.
     alphabetcombinations(words, intemp, emptyspaces, empty);
     set<string> permutewords;
+    
+    //Second for loop
+    //To iterate through every string combinations and permute them.
+    //This makes every possible string
     for(set<string>::iterator itr = words.begin(); itr != words.end(); ++itr)
     {
         permutations(permutewords, inlist, *itr, 0, itr->size()-1);
     }
     set<string> results;
+    
+    //Third for loop
+    //Dictionary Search
     for(set<string>::iterator itr = permutewords.begin(); itr != permutewords.end(); ++itr)
     {
         if(dict.find(*itr)!=dict.end())
@@ -77,6 +89,8 @@ void alphabetcombinations(set<string>& words, string str, vector<int> emptyspace
     }
     string alphabet = "abcdefghijklmnopqrstuvwxyz";
     int temp;
+    
+    //Fourth for loop
     for(int i = 0; i < 26; i++)
     {
         str[emptyspaces[empty-1]] = alphabet[i];
@@ -95,6 +109,8 @@ void permutations(set<string>& set, ::set<int> inlist, string word, int l, int r
     char tempchar;
     if(inlist.find(l) == inlist.end())                        //only swapping floating elements
     {
+        
+        //Fifth and last loop
         for(int i = l; i <= r; i++)
         {
             if(inlist.find(i) == inlist.end())                //only swapping floating elements
